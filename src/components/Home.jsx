fd516bd08d34d4b3a560e18ac8f5e824ba46bfb1
BertoPolo <bertopolo91@gmail.com>
2023-09-05 22:42:58 +0200

    .

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap"
+
+const Home = () => {
+
+    return (
+
+        <Container>
+            <p>hello</p>
+        </Container>
+    )
+}
+
+export default Home
