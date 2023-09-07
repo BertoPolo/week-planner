bd11ddf8f75b55bd6ba89898ccb757b030eef0fe
BertoPolo <bertopolo91@gmail.com>
2023-09-07 16:51:07 +0200

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,20 @@ const Home = () => {
 
     const [weeknum, setWeeknum] = useState(0)
     const [monthDay, setMonthDay] = useState(1)
+    const [monTasks, setMonTasks] = useState([])
+    const [TueTasks, setTueTasks] = useState([])
+    const [wedTasks, setWedTasks] = useState([])
+    const [thuTasks, setThuTasks] = useState([])
+    const [friTasks, setFriTasks] = useState([])
+    const [satTasks, setSatTasks] = useState([])
+    const [sunTasks, setSunTasks] = useState([])
+
+
+    // function to get the week number
+
+    // function to add task to the you want ( dynamic day)
+
+    // 
 
     return (
 
@@ -17,13 +31,13 @@ const Home = () => {
             </Container>
 
 
-
+            {/* this body will be filled by a map and a state([]) */}
             <Card style={{ width: '18rem' }}>
                 <Card.Body>
                     <Card.Title>Monday - {monthDay}</Card.Title>
                     <div>
-                        <input type="checkbox" name="" id="" />
-
+                        <input type="checkbox" name="dynStateName" id="dynStateID" />
+                        <label htmlFor="dynStateName">done or not</label>
                     </div>
                 </Card.Body>
             </Card>
