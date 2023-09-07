c3123f3c254749af701112ef355587d33db21dac
BertoPolo <bertopolo91@gmail.com>
2023-09-07 13:35:26 +0200

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,33 @@
-import { Container } from "react-bootstrap"
+import { useState } from "react"
+import { Container, Row, Card } from "react-bootstrap"
 
 const Home = () => {
 
+    const [weeknum, setWeeknum] = useState(0)
+    const [monthDay, setMonthDay] = useState(1)
+
     return (
 
-        <Container>
-            <p>hello</p>
-        </Container>
+        <>
+            <Container>
+                <Row>
+                    <h3>Weekly Planner</h3>
+                    <h4>Week nº {weeknum}</h4>
+                </Row>
+            </Container>
+
+
+
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>Monday - {monthDay}</Card.Title>
+                    <div>
+                        <input type="checkbox" name="" id="" />
+
+                    </div>
+                </Card.Body>
+            </Card>
+        </>
     )
 }
 
